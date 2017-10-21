426d66acd410b5872f87f17aa0c255ca79591c05
allipierre <yottiallipierre40@yahoo.fr>
2017-10-21 10:13:20 +0200

    test

--- server.js
+++ server.js
@@ -220,7 +220,7 @@ io.sockets.on('connection', function (socket) {
       });
 
       socket.on('messagexy', function (message) {
-      socket.broadcast.emit('messagexyz', 'Merci Piere on cest update');
+      socket.emit('messagexyz', 'Merci Piere on cest update');
       console.log('messagexyz'+ 'Merci Piere on cest update');
           });
 
